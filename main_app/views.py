a16259459d44067d8288211574ecbed82b817684
daceyforward <dacey.loving@gmail.com>
2023-09-12 15:09:00 -0400

    adds finches index and finch dummy data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Lilo', 'breed': 'house', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Zach', 'breed': 'zebra', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 
 # Define the home view
@@ -10,3 +15,8 @@ def home(request):
 # Define the about view
 def about(request):
     return render(request, 'about.html')
+
+# Add index view
+def finches_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finches/index.html', {'finches': finches})
