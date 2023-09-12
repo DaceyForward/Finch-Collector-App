17df9937f6cd6dc39a98047c4bf67b80c55fba78
daceyforward <dacey.loving@gmail.com>
2023-09-12 14:42:02 -0400

    adds about page

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
     return render(request, 'home.html')
+
+# Define the about view
+def about(request):
+    return render(request, 'about.html')
