ef87f8327ec1963de791c1dec72912a56dba46e8
daceyforward <dacey.loving@gmail.com>
2023-09-12 20:29:53 -0400

    adds models and admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-finches = [
-  {'name': 'Lilo', 'breed': 'house', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Zach', 'breed': 'zebra', 'description': 'gentle and loving', 'age': 2},
-]
+# old finch list - will add new via admin portal/models
+# finches = [
+#   {'name': 'Lilo', 'breed': 'house', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Zach', 'breed': 'zebra', 'description': 'gentle and loving', 'age': 2},
+# ]
+
+from .models import Finch
 
 # Create your views here.
 
@@ -19,4 +22,6 @@ def about(request):
 # Add index view
 def finches_index(request):
   # We pass data to a template very much like we did in Express!
-  return render(request, 'finches/index.html', {'finches': finches})
+    finches = Finch.objects.all() # Retrieve all cats
+    return render(request, 'finches/index.html', { 'finches': finches })
+
