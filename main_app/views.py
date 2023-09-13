adc384e832a79905d76ed08dae95300c9d95d2d8
daceyforward <dacey.loving@gmail.com>
2023-09-12 20:47:00 -0400

    adds details page for finches

--- main_app/views.py
+++ main_app/views.py
@@ -25,3 +25,6 @@ def finches_index(request):
     finches = Finch.objects.all() # Retrieve all cats
     return render(request, 'finches/index.html', { 'finches': finches })
 
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
