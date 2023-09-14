a4a0aaf3d4ecfc0c95c0a775d3d09dcfff67c338
daceyforward <dacey.loving@gmail.com>
2023-09-14 14:42:53 -0400

    adds toys to finches and their detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
     path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    # eventually, once it's all set up, we'll add two views to handle the relationship
+    path('finches/<int:finches_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    path('finches/<int:finches_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
  ]
